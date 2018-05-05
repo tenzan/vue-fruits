04fddb124a4b1d3693f0f5e4440688826d87d3a5
Askar Karasaev <askar.karasaev@gmail.com>
2018-05-05 20:52:35 +0900

    Configuring firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App';
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyAXPxcDZqVQv2GY1fvidEm-Pbzxtk-0AWY",
+    authDomain: "vue-fruits.firebaseapp.com",
+    databaseURL: "https://vue-fruits.firebaseio.com",
+    projectId: "vue-fruits",
+    storageBucket: "vue-fruits.appspot.com",
+    messagingSenderId: "559368703703"
+};
+firebase.initializeApp(config);
 
 new Vue({
     render: h => h(App)
